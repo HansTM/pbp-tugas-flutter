ca676385f8957f3802d47b16f390fd295afb7a9d
Hans TM <112265332+HansTM@users.noreply.github.com>
2022-11-08 14:51:19 +0700

    Tugas 7: Buat teks atas dinamis

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 0; 
 
   void _incrementCounter() {
     setState(() {
@@ -53,8 +53,11 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              _counter % 2 == 1 ? "GANJIL" : "GENAP",
+              style: TextStyle(
+                color: _counter % 2 == 1 ? Colors.red : Colors.blue
+              )
             ),
             Text(
               '$_counter',
