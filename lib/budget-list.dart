4a1cc08c24025ed0b5427b1cd15f8da0f5d643a7
Hans TM <112265332+HansTM@users.noreply.github.com>
2022-11-15 00:05:25 +0700

    Tugas 8: Isi halaman daftar

--- lib/budget-list.dart
+++ lib/budget-list.dart
@@ -1,3 +1,4 @@
+import 'package:counter_7/budget-data.dart';
 import 'package:counter_7/drawer.dart';
 import 'package:flutter/material.dart';
 
@@ -18,7 +19,55 @@ class _BudgetListPageState extends State<BudgetListPage> {
         title: Text(widget.title),
       ),
       drawer: const GlobalDrawer(),
-      body: Center(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: budgetList.map((BudgetEntry item) {
+              return Card(
+                child: ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            item.judul,
+                            style: const TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              item.nominal.toString(),
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text(
+                              item.tipe
+                            )
+                          ]
+                        ),
+                        
+                      ],
+                    )
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Text(item.tanggal.toString()),  
+                  ),
+                ),
+              );
+            }).toList()
+          ),
+        ),
       ),
     );
   }
